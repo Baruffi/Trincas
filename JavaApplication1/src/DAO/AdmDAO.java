9fc68c26e52ddb38361ba247550dce665a6062eb
aluno7 <aluno7@unilab.net>
2019-05-22 19:32:13 -0300

    merge mateus

---
+++ JavaApplication1/src/DAO/AdmDAO.java
@@ -0,0 +1,127 @@
+
+package DAO;
+import Model.Adm;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class AdmDAO {
+ 
+public void create (Adm a) {
+    Connection con = Conexao.getConnection();
+    PreparedStatement stmt = null;
+    
+    try{
+            stmt = con.prepareStatement("INSERT INTO usuario (login,senha,nome,formacao) VALUES (?,?,?,?)");
+                 
+            
+            stmt.setString(1,a.getLogin()); 
+            stmt.setString(2,a.getSenha());
+            stmt.setString(3,a.getNome());
+            stmt.setString(4,a.getFormacao());
+        
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso!");
+
+        }
+        catch(SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null,"Erro ao salvar!"+ex);
+        }
+        finally
+        {
+            Conexao.closeConnection(con,stmt);
+        }
+
+}
+
+// -------------------------------------------------------------------------
+
+public void update(Adm a){
+        
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+       
+        try{
+            stmt = con.prepareStatement("UPDATE usuario SET login = ?, senha = ?, nome = ?, formacao = ?,WHERE id_usuario = ?");
+                 
+            stmt.setString(1,a.getLogin());
+            stmt.setString(2,a.getSenha());
+            stmt.setString(3,a.getNome());
+            stmt.setString(4,a.getFormacao());
+            stmt.setInt(5,a.getId_usuario());
+   
+            stmt.executeUpdate();
+                 
+            JOptionPane.showMessageDialog(null, "Atualizado com sucesso!");
+        } 
+        catch(SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null,"Erro ao atualizar!"+ex);
+        }
+        finally
+        {
+            Conexao.closeConnection(con,stmt);
+        }
+    }
+    
+    //=============================================================================
+
+public void delete(Adm a) {
+        
+        Connection con=Conexao.getConnection();
+        PreparedStatement stmt =null;
+        
+        try{
+            stmt = con.prepareStatement("DELETE FROM usuario WHERE id_usuario = ?");
+
+            stmt.setInt(1,a.getId_usuario());
+
+            stmt.executeUpdate();         
+            
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Erro ao excluir"+ex);
+        }
+        finally
+        {
+            Conexao.closeConnection(con, stmt);
+        }
+    }
+
+
+    public void procurar (Adm a) {
+
+Connection con=Conexao.getConnection();
+        PreparedStatement stmt =null;
+        ResultSet rs = null;
+
+        try{
+        stmt = con.prepareStatement("SELECT * FROM usuario WHERE id_usuario = ?");
+
+            stmt.setInt(1, a.getId_usuario());
+            
+            rs = stmt.executeQuery();
+            
+            while(rs.next()) {
+                
+            }
+
+                     
+            
+            JOptionPane.showMessageDialog(null, "Encontrado com sucesso!");
+        }
+        
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Erro ao Encontrar"+ex);
+        }
+        finally
+        {
+            Conexao.closeConnection(con, stmt, rs);
+        }
+
+}
+
+
+}
---
+++ JavaApplication1/src/model/Adm.java
@@ -0,0 +1,65 @@
+
+package Model;
+
+
+public class Adm {
+   
+    
+ private int id_usuario;
+ private String login;
+ private String senha;
+ private String nome;
+ private String formacao;
+
+   
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    
+    public String getLogin() {
+        return login;
+    }
+
+   
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+   
+    public String getSenha() {
+        return senha;
+    }
+
+    
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    public String getFormacao() {
+        return formacao;
+    }
+
+    
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+
+    
+}
