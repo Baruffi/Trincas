38541bf46c1c75df622f661866265258461b9343
Raphael <raphabaruffi@gmail.com>
2019-06-12 19:08:05 -0300

    Update JCadMaterial.java

--- JavaApplication1/src/view/JCadMaterial.java
+++ JavaApplication1/src/view/JCadMaterial.java
@@ -419,7 +419,7 @@ public class JCadMaterial extends javax.swing.JFrame {
                 txtEspessura.setText("");
                 readjTabMateriais();
         }
-    }
+    }                                        
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         Material m = new Material();
